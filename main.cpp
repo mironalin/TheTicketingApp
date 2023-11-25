7f28a1bef4340c3dc739c13bae42b3da1eeef286
mironalin <alinmiron3@gmail.com>
2023-11-25 07:26:18 -0800

    Initialize the MENU

--- main.cpp
+++ main.cpp
@@ -14,13 +14,27 @@ int main() {
     // User input for Event
     std::cin >> event;
 
-    //Display Location details
+    // Display Location details
     std::cout << "\nLocation Details:\n";
     location.displayLocationDetails();
 
-    //Display Event details
+    // Display Event details
     std::cout << "\nEvent Details:\n";
     event.displayEventDetails();
+
+    // Menu-driven options
+    int choice;
+    do {
+        std::cout << "\n========== MENU ==========";
+        std::cout << "\n1. Generate Ticket \n";
+        std::cout << "\n2. Validate Ticket \n";
+        std::cout << "\n3. Exit \n";
+        std::cout << "========== MENU ==========\n";
+        std::cout << "\nEnter your choice: ";
+        std::cin >> choice;
+
+
+    } while (choice != 3);
 
     return 0;
 }
